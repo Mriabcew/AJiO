f0307fe942c574ae38a103beda9ba3ce30363078
Michał Riabcew <rudy2305@icloud.com>
2023-12-12 22:56:15 +0100

    Dodaj pliki projektów.

---
+++ App.SQLQuerryClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using App.SQLQueryEngine;
+
+namespace App.SQLQuerryClient
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Tworzymy instancję klasy SQLEngine
+            SQLEngine sqlEngine = new SQLEngine("Auctions");
+
+            // Ustawiamy zapytanie NLP
+            sqlEngine.NPLQuery = @"Komputery stacjonarne od 2500 do 3000 zł";
+
+            // Pobieramy zapytanie SQL
+            string sqlQuery = sqlEngine.GetSQLQuery();
+
+            // Wyświetlamy wynik
+            Console.WriteLine($"NPL Query: {sqlEngine.NPLQuery}\n\n");
+            Console.WriteLine($"Generated SQL Query: {sqlQuery}");
+        }
+    }
+
+}
---
+++ App.SQLQuerryClient/SQLEngine.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace App.SQLQueryEngine
+{
+    public class SQLEngine
+    {
+        public string NPLQuery { get; set; }
+        public string SQLQuery { get; set; }
+
+        private string TableName { get; set; } 
+
+        private string endChars = @"aiy";
+        private string[] targetWords = { "od", "do", "poniżej", "powyżej" };
+
+        public SQLEngine(string tableName)
+        {
+            TableName = tableName;
+        }
+
+        public string GetSQLQuery()
+        {
+            if (string.IsNullOrWhiteSpace(NPLQuery))
+                return "Error: input is empty or contains only whitespace";
+            NPLQuery = NPLQuery.TrimStart();
+            ProcessNLPQuery();
+            return SQLQuery;
+        }
+
+        private void ProcessNLPQuery()
+        {
+            string[] words = SplitToWords();
+            int indexOfFirstTargetWord = words
+                .Select((word, index) => new { Word = word, Index = index })
+                .FirstOrDefault(item => targetWords.Contains(item.Word.ToLower()))?.Index ?? -1;
+
+            // Początkowe zapytanie SQL
+            SQLQuery = GetDefaultSQLQuery(this.TableName);
+
+            // Domyślny warunek dla ceny
+            string priceCondition = "<";
+
+            int i = 0;
+            string x = null;
+            bool price = false;
+
+            // Jeśli użytkownik nie podał ceny, zainicjuj zmienną, aby uwzględnić wszystkie słowa
+            int indexOfLastWord = words.Length;
+
+            // Jeśli użytkownik podał ceny, ogranicz indeks ostatniego słowa
+            if (indexOfFirstTargetWord != -1)
+            {
+                indexOfLastWord = indexOfFirstTargetWord;
+            }
+
+            // Iteracja po słowach w zapytaniu NLP
+            foreach (var word in words)
+            {
+                // Sprawdź, czy słowo to jedno z kluczowych słów zmieniających warunek ceny
+                if (targetWords.Contains(word.ToLower()))
+                {
+                    // Aktualizuj warunek ceny tylko dla słów kluczowych
+                    priceCondition = word.ToLower() == "do" || word.ToLower() == "poniżej" ? "<" : ">";
+                    price = true;
+                }
+
+                // Jeśli słowo to liczba, ustaw nowy warunek ceny
+                if (IsNumeric(word) && price)
+                {
+                    SQLQuery += $" AND Price {priceCondition} {word}";
+                }
+                else
+                {
+                    if (i < indexOfLastWord)
+                    {
+                        if (endChars.Contains(word[word.Length - 1]))
+                        {
+                            x = word.Substring(0, word.Length - 1);
+                        }
+                        else
+                        {
+                            x = word;
+                        }
+
+                        // Jeśli słowo to nie liczba, dodaj do warunku LIKE dla tytułu i opisu
+                        SQLQuery += $" AND (Title LIKE '%{x}%' OR Description LIKE '%{x}%')";
+                    }
+                }
+
+                i++;
+            }
+        }
+
+        private string[] SplitToWords()
+        {
+            return NPLQuery.Split(' ');
+        }
+
+        public static string GetDefaultSQLQuery(string tableName)
+        {
+            return $"SELECT * FROM {tableName} WHERE 1=1";
+        }
+
+        private static bool IsNumeric(string input)
+        {
+            return double.TryParse(input, out _);
+        }
+    }
+}
